008f591a5877cd5b3caac8e96796788c9f6fa6b5
CBT-Hjemme <Christianbt96@gmail.com>
2023-09-26 19:59:25 +0200

    Objects basics

---
+++ Projekter/Objects/script.js
@@ -0,0 +1,60 @@
+
+
+// 📝 Exercise 1 - level 1
+// The objects below have some syntax issues - try and fix them all!
+
+const kitten = {
+    fur_colour: "orange",
+    age: "23"
+};
+
+const laptop = {
+    brand: "Lenovo",
+    ram: "5GB"
+};
+
+const phone = {
+    operating_system: "iOS",
+    hasStylus: true,
+    megapixels: 12,
+    batteryLife: "24 hours"
+};
+
+// 📝 Exercise 2 - level 1
+// Create an object that describes you. Fx a key called age with the value of 28
+
+
+
+// 📝 Exercise 3 - level 1
+// Log the values of each property of kitten
+// console.log the values of each property of "kitten"
+let kitten2 = {
+    ageMonths: 3,
+    isFemale: true,
+    furColour: "brown",
+};
+
+console.log(kitten2)
+// console.log(kitten2.ageMonths)
+// console.log(kitten2.isFemale)
+// console.log(kitten2.furColour)
+// console.log(kitten2["ageMonths"])
+// console.log(kitten2["isFemale"])
+// console.log(kitten2["furColour"])
+// Can use "variable.thing" method or "variable["thing"]"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
