9964110dc8ad990d292aed5d7cb13f3d0cea63ef
ChristianBT96 <Christianbt95@gmail.com>
2023-10-06 09:03:45 +0200

    Code from class

--- Projekter/DOM Manipulation/script.js
+++ Projekter/DOM Manipulation/script.js
@@ -2,22 +2,28 @@
 // DOM - Document Object Model
 // Manipulate Content, Style, and Structure
 
-//  DOM Manipulation
+// Access
+const h1Element = document.querySelector("h1")
+
+console.log(h1Element)
 
 
+// Change Manipulate
 
+h1Element.textContent = "something"
 
+// Create
 
+const liLastElement = document.querySelector(".last-element")
+console.log(liLastElement)
+liLastElement.insertAdjacentElement("afterend", "<li>Strawberry</li>")
 
+// Event
 
+const button = document.querySelector("button")
 
-
-
-
-
-
-
-
+button.addEventListener("click", function () {
+    console.log("Hej")})
 
 
 
