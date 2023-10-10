1e7b207f090455e92d17af78319e1a416cae5453
CBT-Hjemme <Christianbt96@gmail.com>
2023-10-10 17:25:03 +0200

    Added a lot

--- Projekter/DOM 2/script.js
+++ Projekter/DOM 2/script.js
@@ -1,45 +1,41 @@
 
-// Click button and credit count //
+// // // Click button and credit count/amount // // //
 
 const clickButton = document.querySelector(".main-click");
 const creditCount = document.querySelector(".credit-count");
+let creditAmount = parseInt(creditCount.textContent, 10);
 
-// console.log(parseInt(creditCount, 10));
-
-let credits = parseInt(creditCount.textContent, 10);
-
+// Add 1 credit to credit count when button is clicked //
 clickButton.addEventListener("click", () => {
-    credits += 1;
-    creditCount.textContent = credits;
-    upgradeButtonUnlock(credits);
+    creditAmount += 1;
+    creditCount.textContent = creditAmount;
 });
 
 //  DONE //
 
 
 
-// Unlock upgrades button //
+// // //  Unlock upgrades button // // //
 
 const unlockUpgradesButton = document.querySelector(".upgrades-unlock");
 
 // Function to make upgrade button visible when credits are above 30 //
-const upgradeButtonUnlock = (credits) => {
-    if (credits >= 30) {
+const upgradeButtonUnlock = () => {
+    if (creditAmount >= 5) {
         unlockUpgradesButton.style.opacity = 1;
+        unlockUpgradesButton.style.cursor = "pointer";
         unlockUpgradesButton.style.pointerEvents = "auto";
     }
 }
 
+// Check if credits are above 30 every half a second //
+setInterval(upgradeButtonUnlock, 500);
+
 //  DONE //
 
-// Upgrade buttons and objects //
+// // // Upgrade buttons and objects // // //
 
-
-unlockUpgradesButton.addEventListener("click", () => {
-
-});
-
-
+// Create upgrade objects //
 const upgradeObjects = {
     Upgrades:[
         {
@@ -53,7 +49,7 @@ const upgradeObjects = {
             amount: 0,
         },
         {
-            name: "Fuel Generator",
+            name: "Fuel Extractor",
             price: 100,
             amount: 0,
         },
@@ -64,42 +60,81 @@ const upgradeObjects = {
         }]
 };
 
+const upgradeDiv = document.querySelector(".upgrades");
 
-// unlockUpgradesButton.addEventListener("click", () => {
-//     if (credits >= 40) {
-//
-//     }
-//
-//
-// });
 
-console.log(upgradeObjects);
+unlockUpgradesButton.addEventListener("click", () => {
 
-// const upgradeButtons = document.querySelectorAll(".upgrade");
-// const upgradePrices = document.querySelectorAll(".upgrade-price");
-//
-// const testButton = document.querySelector(".test");
-//
-// testButton.addEventListener("click", () => {
-//     upgradePrices.forEach((price) => {
-//         price.textContent = 50;
+
+    if (creditAmount >= 5) {
+        const upgradeH2 = document.createElement("h2");
+        upgradeH2.textContent = "Upgrades:";
+        upgradeDiv.append(upgradeH2)
+        unlockUpgradesButton.style.visibility = "hidden";
+
+        upgradeObjects.Upgrades.forEach((upgrade, index) => {
+
+            // Count
+            const upgradeCount = document.createElement("p");
+            upgradeCount.textContent = "Count: " + upgrade.amount;
+            upgradeDiv.append(upgradeCount);
+            // Price tag
+            const upgradePrice = document.createElement("p");
+            upgradePrice.textContent = "Price: " + upgrade.price + " credits";
+            upgradeDiv.append(upgradePrice);
+            // Button creation
+            const upgradeButton = document.createElement("button");
+            upgradeButton.style.opacity = "0.5";
+            upgradeButton.style.pointerEvents = "none";
+            upgradeButton.textContent = upgrade.name;
+            upgradeButton.classList.add(`upgrade-button`);
+            upgradeDiv.append(upgradeButton);
+        });
+
+        creditAmount -= 5;
+        creditCount.textContent = creditAmount;
+
+
+    }
+});
+
+
+
+
+
+//  DONE //
+
+// // // Button listener // // //
+
+// Create button listener //
+
+const getButtons = document.querySelectorAll(".upgrade-button");
+const upgradePrice = document.querySelectorAll("p:nth-child(2)");
+console.log(upgradePrice);
+
+// const fadeButtons = () => {
+//     getButtons.forEach((button) => {
+//         if () {
+//             button.style.opacity = 1;
+//             button.style.pointerEvents = "auto";
+//         }
 //     });
-// });
+// };
+// setInterval(fadeButtons, 500);
+
+// DONE //
+
+
+
+// // //  // // //
+
+
 //
-//
-//
-//
-// console.log(upgradeButtons);
-// console.log(upgradePrices);
-
-
-
-
-
-
-
-
-
+// upgradeDiv.style.display = "flex";
+// upgradeDiv.style.flexDirection = "column";
+// upgradeDiv.style.justifyContent = "space-evenly";
+// upgradeDiv.style.flexWrap = "wrap";
+// upgradeDiv.style.alignItems = "center";
 
 
 
