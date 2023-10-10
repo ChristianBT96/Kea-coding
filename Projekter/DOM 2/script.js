889059a1a026da96b2ada5b9073e59ecd4a4d673
CBT-Hjemme <Christianbt96@gmail.com>
2023-10-10 13:57:48 +0200

    Added click and non-functioning upgrades

--- Projekter/DOM 2/script.js
+++ Projekter/DOM 2/script.js
@@ -1,21 +1,17 @@
 
-
-
-const creditSpan = document.querySelector(".credit-counter").textContent
-
-console.log(creditSpan)
-
-// creditSpan.addEventListener("click", {
-//
-// })
-//
+const clickButton = document.querySelector(".main-click");
+const creditCount = document.querySelector(".credit-count");
 
 
 
+let credits = 0;
 
+clickButton.addEventListener("click", () => {
+    credits += 1;
+    creditCount.textContent = credits;
+});
 
-
-
+const upgradeButtons = document.querySelectorAll(".upgrade");
 
 
 
