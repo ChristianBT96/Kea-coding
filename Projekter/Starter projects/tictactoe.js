e7f5bb103e4e09d2660301f149771b2744ec9089
CBT-Hjemme <Christianbt96@gmail.com>
2023-10-18 18:27:38 +0200

    Click is yes

--- Projekter/Starter projects/tictactoe.js
+++ Projekter/Starter projects/tictactoe.js
@@ -1,24 +1,40 @@
 
 
-// const rules = document.querySelector('.rules');
-//
-// rules.addEventListener("mouseover", () => {
-//
-// });
 
 
 
+// // // // Input Fields // // // //
 
+// 1. Get the input field
+const field1 = document.querySelector('#field-1');
+const field2 = document.querySelector('#field-2');
+const field3 = document.querySelector('#field-3');
+const field4 = document.querySelector('#field-4');
+const field5 = document.querySelector('#field-5');
+const field6 = document.querySelector('#field-6');
+const field7 = document.querySelector('#field-7');
+const field8 = document.querySelector('#field-8');
+const field9 = document.querySelector('#field-9');
 
+field1.addEventListener('click', () => {
+    if (field1.innerHTML === "") {
+      field1.innerHTML = 'X';
+    } else if (field1.innerHTML === "X") {
+      field1.innerHTML = 'O';
+    } else {
+      field1.innerHTML = "";
+    }
+});
 
-
-
-
-
-
-// // // // // // // //
-
-
+field2.addEventListener('click', () => {
+    if (field2.innerHTML === "") {
+      field2.innerHTML = 'X';
+    } else if (field2.innerHTML === "X") {
+      field2.innerHTML = 'O';
+    } else {
+      field2.innerHTML = "";
+    }
+});
 
 // // // // // // // //
 
